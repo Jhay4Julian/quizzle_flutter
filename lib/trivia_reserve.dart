a49d2f4a8e597bf9b4ec2977e20c9673c380cc90
JulianMozi <Jkoripamo@gmail.com>
2022-06-01 21:46:27 +0100

    moved the list of questions to the TriviaBank class in the trivia_reserve.dart file

---
+++ lib/trivia_reserve.dart
@@ -0,0 +1,13 @@
+import 'package:quizzle/question.dart';
+
+class TriviaBank {
+  List<Question> triviaBank = [
+    Question(q: 'Alexander Fleming discovered penicillin.', a: true),
+    Question(q: 'The black box in a plane is black.', a: false),
+    Question(q: 'Harry Styles\' middle name is Edward.', a: true),
+    Question(q: 'There are five different blood groups.', a: false),
+    Question(q: 'There are 14 bones in a human foot.', a: false),
+    Question(q: 'Meghan Markle\'s first name is Rachel.', a: true),
+    Question(q: 'Coffee is made from berries.', a: true),
+  ];
+}
