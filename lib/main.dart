1d39484ecbaa491186c66e5ce95da13ead029da3
JulianMozi <Jkoripamo@gmail.com>
2022-06-01 23:41:52 +0100

    added the triviaBank object to the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzle/question.dart';
+import 'package:quizzle/trivia_reserve.dart';
+
+TriviaBank triviaBank = TriviaBank();
 
 void main() {
   runApp(const Quizzle());
@@ -33,16 +35,6 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-
-  List<Question> triviaBank = [
-    Question(q: 'Alexander Fleming discovered penicillin.', a: true),
-    Question(q: 'The black box in a plane is black.', a: false),
-    Question(q: 'Harry Styles\' middle name is Edward.', a: true),
-    Question(q: 'There are five different blood groups.', a: false),
-    Question(q: 'There are 14 bones in a human foot.', a: false),
-    Question(q: 'Meghan Markle\'s first name is Rachel.', a: true),
-    Question(q: 'Coffee is made from berries.', a: true),
-  ];
   
   int questionNumber = 0;
 
